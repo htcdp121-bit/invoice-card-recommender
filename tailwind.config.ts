870a7f6cd558b0839e73cd683bda295e56d0849d
htcdp121-bit <htcdp121@gmail.com>
2026-05-05 15:09:04 +0800

    chore: add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './app/**/*.{ts,tsx,js,jsx,mdx}',
+    './components/**/*.{ts,tsx,js,jsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
